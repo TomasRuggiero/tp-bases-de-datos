a9a50306cac1821ec5aeef68199383523c733428
Manu Cabanas <manu.cabanas@hotmail.com>
2025-06-25 12:14:00 -0300

    Refactor Hecho_pedido

--- script_creacion_BI.sql
+++ script_creacion_BI.sql
@@ -61,17 +61,50 @@ CREATE TABLE THIS_IS_FINE.BI_sucursal(
 )
 
 CREATE TABLE THIS_IS_FINE.BI_Hecho_Pedido(
+	pedido_id INT,
 	pedido_sucursal INT,
 	pedido_tiempo INT,
 	pedido_rango_etario INT,
-	pedido_horario_ventas INT,
+	pedido_turno_ventas INT,
 	pedido_modelo_sillon INT,
 	pedido_estado INT,
 	pedido_fecha datetime2(6),
-	pedido_precio_total decimal(18,2)
+	pedido_cantidad INT,
+	pedido_precio_total decimal(18,2),
+
+	CONSTRAINT PK_Hecho_pedido PRIMARY KEY (pedido_id),
+
 	CONSTRAINT FK_Hecho_Pedido_sucursal FOREIGN KEY (pedido_sucursal)
-		REFERENCES THIS_IS_FINE.BI_sucursal (sucursal_codigo)
+		REFERENCES THIS_IS_FINE.BI_sucursal (sucursal_codigo),
+	CONSTRAINT FK_Hecho_Pedido_tiempo FOREIGN KEY (pedido_tiempo)
+		REFERENCES THIS_IS_FINE.BI_tiempo (tiempo_codigo),
+	CONSTRAINT FK_Hecho_Pedido_rango_etario FOREIGN KEY (pedido_rango_etario)
+		REFERENCES THIS_IS_FINE.BI_rango_etario (rango_etario_codigo),
+	CONSTRAINT FK_Hecho_Pedido_horario_ventas FOREIGN KEY (pedido_turno_ventas)
+		REFERENCES THIS_IS_FINE.BI_turno_ventas (turno_codigo),
+	CONSTRAINT FK_Hecho_Pedido_modelo_sillon FOREIGN KEY (pedido_modelo_sillon)
+		REFERENCES THIS_IS_FINE.BI_modelo_sillon (modelo_codigo),
+	CONSTRAINT FK_Hecho_Pedido_estado FOREIGN KEY (pedido_estado)
+		REFERENCES THIS_IS_FINE.BI_estado_pedido (estado_codigo)
 )
+
+CREATE TABLE THIS_IS_FINE.BI_Hecho_factura(
+	factura_sucursal INT,
+	factura_tiempo INT,
+	factura_modelo_sillon INT,
+	factura_cantidad INT,
+	factura_fecha datetime2(6),
+	factura_total decimal(12,2)
+	CONSTRAINT PK_Hecho_Factura PRIMARY KEY (factura_sucursal, factura_tiempo, factura_modelo_sillon)
+	CONSTRAINT FK_Hecho_Factura_sucursal FOREIGN KEY (factura_sucursal)
+		REFERENCES THIS_IS_FINE.BI_sucursal (sucursal_codigo),
+	CONSTRAINT FK_Hecho_Factura_tiempo FOREIGN KEY (factura_tiempo)
+		REFERENCES THIS_IS_FINE.BI_tiempo (tiempo_codigo),
+	CONSTRAINT FK_Hecho_Factura_modelo_sillon FOREIGN KEY (factura_modelo_sillon)
+		REFERENCES THIS_IS_FINE.BI_modelo_sillon (modelo_codigo)
+)
+
+CREATE TABLE THIS_IS_FINE.BI_Hecho_
 
 
 
